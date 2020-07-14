df7edd03148f3505a9f1464c795205bd60bea940
ariana-mirus <arianamirus@gmail.com>
2020-07-15 08:09:14 +1200

    cleaned up app.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,30 +2,11 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addToCountAction } from '../actions/index'
 
-// changeHandler = (evt) => {
-
-// }
-
-// addToCount = (count) => {
-//   this.setState({
-//     count: count
-//   })
-// }
-
-// componentDidMount () {
-//   const { addToCount } = this.props
-//   addToCount()
-// }
-
-// onClick = () => {
-//   increaseCount(this.state)
-// }
-
 function App ({ addToCountAction, myCount }) {
   return (
     <div className="App">
-      <button onClick={addToCountAction}
-      >{ myCount }
+      <button onClick={addToCountAction}>
+        { myCount }
       </button>
     </div>)
 }
